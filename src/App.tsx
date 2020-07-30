4d7a8af917f45d58f0e9ffb6fd866336682d11c8
jacade <j-c.dette@arcor.de>
2020-07-30 20:05:11 +0200

    Added function

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Grid from './components/Grid';
+import { GridData, GRID_X, GRID_O, PlayerAtTurn, GridValue, GridRow } from './types';
+import { stat } from 'fs';
+
+export interface State {
+  data: GridData,
+  turn: PlayerAtTurn
+}
+
+const flipTurn: (currentTurn: PlayerAtTurn) => PlayerAtTurn = (currentTurn) => currentTurn === GRID_X ? GRID_O : GRID_X;
+
+const makeMove: (row: number, col: number, data: GridData, atTurn: PlayerAtTurn) => GridData = (row, col, data, atTurn) =>
+  data.map<GridRow>((r, i) => i !== row ? r : r.map<GridValue>((v, j) => j !== col ? v : atTurn) as GridRow) as GridData;
 
 function App() {
+  const [state, setState] = useState<State>({
+    data: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    turn: GRID_X
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh" }}>
+      <Grid data={state.data} onBoxClick={(row, col) => setState({
+        ...state,
+        data: makeMove(row, col, state.data, state.turn),
+        turn: flipTurn(state.turn)
+      })} />
     </div>
   );
 }
