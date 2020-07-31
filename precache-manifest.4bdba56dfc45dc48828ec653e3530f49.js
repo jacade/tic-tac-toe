9e75efae148e6682de8dbca00847e0dfc2a52d3e
jacade <j-c.dette@arcor.de>
2020-07-31 09:17:51 +0200

    Updates

---
+++ precache-manifest.4bdba56dfc45dc48828ec653e3530f49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd243fde47ee3c8fea2f6598e3deebe5",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f9ca96ccb29e948b9755",
+    "url": "/tic-tac-toe/static/css/main.db71148d.chunk.css"
+  },
+  {
+    "revision": "e0b76281c4f41d6a212e",
+    "url": "/tic-tac-toe/static/js/2.62bcb7fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.62bcb7fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ca96ccb29e948b9755",
+    "url": "/tic-tac-toe/static/js/main.8a1b4e98.chunk.js"
+  },
+  {
+    "revision": "3a8626f9d3163928b6c9",
+    "url": "/tic-tac-toe/static/js/runtime-main.41fa67c6.js"
+  }
+]);
